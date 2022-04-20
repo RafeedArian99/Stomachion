2580202dc3bf23ac7ae9ad05f6e09ddf6e8fa7df
Rafeed Arian <rafeedarian99@gmail.com>
2022-04-19 18:52:04 -0700

    Added more classes and contents

--- src/View/View.java
+++ src/View/View.java
@@ -1,4 +1,23 @@
 package View;
 
-public class View {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class View extends Application implements Observer {
+    public static void main(String args) {
+
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
 }
