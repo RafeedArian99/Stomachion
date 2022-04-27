001fdce057ef6cfa063f490e7d69022fa5cb42fa
Owner <Owner@DESKTOP-1TJNVSF>
2022-04-27 15:37:24 -0700

    I started implementing model and bounding box

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -1,6 +1,36 @@
 package Model;
 
+import java.util.ArrayList;
 import java.util.Observable;
 
+import Utilities.*;
+
 public class Model extends Observable {
+	
+	private BoundingBox mainBox;
+	
+	private BoundingBox selectionBox;
+	
+	public Model() {
+		this.pieceList = new ArrayList<Piece>();
+		for (int i = 0; i < 14; i++) {
+			Piece newPiece = new Piece(i);
+			pieceList.add(newPiece);
+		}
+		this.box = new BoundingBox();
+	}
+	
+	// selection box, takes in two coordinates as arguments, returns 
+	// every piece object, not piece ID, as an ArrayList of pieces,
+	// selects all pieces fully within the bounds.
+	
+	public ArrayList<Piece> selectionBox(double x1, double y1, double x2, double y2) {
+		
+		
+		return this.pieceList;
+	}
+	
+	// plucking removes from the main box and puts into the selection box
+	// placing removes form the selection box and places in the man box.
+	
 }
--- src/Utilities/BoundingBox.java
+++ src/Utilities/BoundingBox.java
@@ -1,4 +1,15 @@
 package Utilities;
 
+import java.util.ArrayList;
+
 public class BoundingBox {
+	
+	private ArrayList<Piece> pieceList;
+	
+	public BoundingBox(double x1, double y1, double width, double height) {
+		
+	}
+	
+	// checkContents, returns all pieces within the coordinates of this box
+	// uses the getAllCoords from the piece method
 }
