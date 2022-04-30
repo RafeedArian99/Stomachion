959f269f0fd94e1173761e4985e2fcb0d3eb1820
Owner <Owner@DESKTOP-1TJNVSF>
2022-04-30 14:49:01 -0700

    implements new features of board and model

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -4,31 +4,62 @@ import java.util.ArrayList;
 import java.util.Observable;
 
 import Utilities.*;
+import Utilities.Piece.PieceState;
 
 public class Model extends Observable {
 	
 	private BoundingBox mainBox;
 	
-	private BoundingBox selectionBox;
+	private boolean selectionHas;
+	
+	private Piece selected;
 	
 	public Model() {
-		this.pieceList = new ArrayList<Piece>();
-		for (int i = 0; i < 14; i++) {
-			Piece newPiece = new Piece(i);
-			pieceList.add(newPiece);
-		}
-		this.box = new BoundingBox();
+		// TODO THIS DOES NOT NEED ANY ARGS NOW
+		this.mainBox = new BoundingBox(0, 0, 36, 36);
 	}
 	
-	// selection box, takes in two coordinates as arguments, returns 
-	// every piece object, not piece ID, as an ArrayList of pieces,
-	// selects all pieces fully within the bounds.
-	
-	public ArrayList<Piece> selectionBox(double x1, double y1, double x2, double y2) {
-		
-		
-		return this.pieceList;
-	}
+    @SuppressWarnings("deprecation")
+	public void pluckPiece(double gridX, double gridY) {
+    	Piece[] array = this.mainBox.getList();
+    	for (int i = 0; i < 14; i++) {
+    		if (array[i].encapsulates(gridX, gridY)) {
+    			array[i].setSelected(true);
+    			this.selectionHas = true;
+    			this.selected = array[i];
+    			break;
+    		}
+    	}
+    	setChanged();
+    	notifyObservers(mainBox.getList());
+    }
+
+    public void highlight(double gridX, double gridY) {
+    	Piece[] array = this.mainBox.getList();
+    	for (int i = 0; i < 14; i++) {
+    		if (array[i].encapsulates(gridX, gridY)) {
+    			array[i].highlight(PieceState.VALID);
+    			break;
+    		}
+    	}
+    	setChanged();
+    	notifyObservers(this.mainBox.getList());
+    }
+
+    public boolean hasPieceSelected() {
+        return this.selectionHas;
+    }
+
+    public void placePiece() {
+    	this.selected.setSelected(false);
+    	this.selectionHas = false;
+    	setChanged();
+    	notifyObservers(this.mainBox.getList());
+    }
+
+    public void checkPlacement(double gridX, double gridY) {
+    	
+    }
 	
 	// plucking removes from the main box and puts into the selection box
 	// placing removes form the selection box and places in the man box.private BoundingBox mainBox;private BoundingBox mainBox;private BoundingBox mainBox;
