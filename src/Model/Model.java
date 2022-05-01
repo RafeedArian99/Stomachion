ff834f673d3d07e871ffb1efdcf213e27b89b61b
Owner <Owner@DESKTOP-1TJNVSF>
2022-05-01 01:03:25 -0700

    test

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -61,6 +61,10 @@ public class Model extends Observable {
     	
     }
 	
+    public void test() {
+    	
+    }
+    
 	// plucking removes from the main box and puts into the selection box
 	// placing removes form the selection box and places in the man box.private BoundingBox mainBox;private BoundingBox mainBox;private BoundingBox mainBox;
 	
