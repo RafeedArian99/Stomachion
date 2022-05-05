890b51d60833500c970b53edae195d03aae20e82
Ethan <ecarrasco3879@aol.com>
2022-05-04 22:29:08 -0700

    junit test works now. Issue solved. User error

--- src/Utilities/StomachionTests.java
+++ src/Utilities/StomachionTests.java
@@ -1,4 +1,9 @@
-package Controller;
+/*
+ * @author: Ethan Carrasco
+ * 
+ * JUnit tests are in here for the controller.
+ */
+package Utilities;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -7,20 +12,20 @@ import java.util.Observer;
 
 import org.junit.jupiter.api.Test;
 
+import Controller.Controller;
 import Model.Model;
 
 class StomachionTests {
 
+	@SuppressWarnings("unused")
 	@Test
 	void testAddObserverToModel() {
-		final int[] a = {999};
-
+		int[] a = {999};
 		ArrayList<int[]> textures = new ArrayList<>();
 		for (int i = 0; i < 14; i++)
 			textures.add(new int[3]);
-
         Controller controller = new Controller((observable, obj) -> a[0] = 444, textures);
-        System.out.println();
+        //System.out.println(a[0]);
         assertEquals(a[0], 444);	
     }
 	
