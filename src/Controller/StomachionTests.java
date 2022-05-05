87f15917a894b2e6d954faeaa1bae0a96ee4fc50
Ethan <ecarrasco3879@aol.com>
2022-05-04 21:29:51 -0700

    junit setup and readme

---
+++ src/Controller/StomachionTests.java
@@ -0,0 +1,42 @@
+package Controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Observer;
+
+import org.junit.jupiter.api.Test;
+
+import Model.Model;
+
+class StomachionTests {
+
+	@Test
+	void testAddObserverToModel() {
+		
+	}
+	
+	void testPluckPiece() {
+		
+	}
+	
+	void testHighlight() {
+		
+	}
+	
+	void testPieceSelectedTrue() {
+		
+	}
+	
+	void testPieceSelectedFalse() {
+		
+	}
+	
+	void testCheckPlacement() {
+		
+	}
+	
+	void testUpdateSelection() {
+		
+	}
+
+}
